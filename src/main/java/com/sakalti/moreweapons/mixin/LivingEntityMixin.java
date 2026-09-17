09f28c48eb494e0aca6e77c0c7bfa4ac3e643533
maikuran <abyssalx@outlook.jp>
2026-09-17 18:15:43 +0900

    Update LivingEntityMixin.java

--- src/main/java/com/sakalti/moreweapons/mixin/LivingEntityMixin.java
+++ src/main/java/com/sakalti/moreweapons/mixin/LivingEntityMixin.java
@@ -1,21 +1,20 @@
 package com.sakalti.moreweapons.mixin;
 
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin {
+@Mixin(Entity.class)
+public abstract class EntityMixin {
 
-    // hurtResistantTime は protected なため Shadow で取得可能
-    @Shadow protected int hurtResistantTime;
+    @Shadow
+    private int timeUntilRegen;
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void removeInvulnerability(CallbackInfo ci) {
-        this.hurtResistantTime = 0;
+        this.timeUntilRegen = 0;
     }
 }
