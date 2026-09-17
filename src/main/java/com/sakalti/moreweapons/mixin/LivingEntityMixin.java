6bded7c520bea509630de64b74858afad74aa658
maikuran <abyssalx@outlook.jp>
2026-09-17 18:29:32 +0900

    Update LivingEntityMixin.java

--- src/main/java/com/sakalti/moreweapons/mixin/LivingEntityMixin.java
+++ src/main/java/com/sakalti/moreweapons/mixin/LivingEntityMixin.java
@@ -1,22 +1,21 @@
 package com.sakalti.moreweapons.mixin;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Entity.class)
-public abstract class EntityMixin {
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
 
     @Shadow
     protected int timeUntilRegen;
 
     @Inject(method = "tick", at = @At("HEAD"))
-    private void removeInvulnerability(CallbackInfo ci) {
-        if ((Object) this instanceof net.minecraft.entity.LivingEntity) {
-            this.timeUntilRegen = 0;
-        }
+    private void moreweapons$removeInvulnerability(CallbackInfo ci) {
+        this.timeUntilRegen = 0;
     }
 }
